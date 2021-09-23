58b424ad20b4d6d5d41bd9b2fd615d900084fd5f
Artem Listopad <rakvay@mail.ru>
2021-09-23 23:07:59 +0300

    IMP Update docker project: - final release

--- docker/my-project/frontend/src/App.js
+++ docker/my-project/frontend/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const makeApiRequest = () => {
+    console.log("makeApiRequest");
+    axios("/api/testwithcurrentuser").then(response => {
+      console.log("response", response);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!!! Hello from inside
-          docker Hi I'm development
+          Websockets fix Edit! <code>src/App.js</code> and save to reload!!!!
+          Hello from inside docker Hi I'm development
         </p>
         <a
           className="App-link"
@@ -20,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={makeApiRequest}>Make api request</button>
     </div>
   );
 }
